7f4d6a5deef8c47c9526a036227702e551b14f6e
hyper1923 <82442186+hyper1923@users.noreply.github.com>
2022-05-29 18:53:33 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 #include <cstring>
 
 
+#define minute 0x1
+#define seconds 0x2
+#define hours 0x3
+#define nothing 0x0
+
 int main(){
     char timeC[12];
+    short dataType = nothing;
     printf("Enter your time (Exampe 50m): "); scanf("%s",&timeC[0]);
 
     uint32_t timestamp = time(0);
@@ -12,12 +18,27 @@ int main(){
     for(int i = 0; i < 12; i++){
         timeStr += timeC[i];
         if(timeC[i] == 'm'){
+            time32 = std::stoi(timeStr) * 60;
+            dataType = minute;
+            break;
+        }
+        if(timeC[i] == 's'){
             time32 = std::stoi(timeStr);
+            dataType = seconds;
+            break;
+        }
+        if(timeC[i] == 'h'){
+            time32 = std::stoi(timeStr)*60*60;
+            dataType = hours;
             break;
         }
     }
 
-    time32 = 60*time32;
+    if(dataType == nothing){
+        printf("You didn't add s,m or h to end of string.\n\r");
+        exit(EXIT_FAILURE);
+    }
+
     uint32_t totalStamp = timestamp + time32;
 
     bool finished = false;
